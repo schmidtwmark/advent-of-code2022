1a6d99e8864c6398c4b4d91229f313fcc038fee7
Mark Schmidt <mark.schmidt@hey.com>
2022-12-03 14:07:43 -0600

    Cleanup

--- template.rs
+++ template.rs
@@ -4,13 +4,13 @@ fn main() {
     let sample = include_str!("../../samples/aaaaa.txt");
     let input = include_str!("../../inputs/aaaaa.txt");
     let part_one_problems = [
-        aoc::Problem::new(sample, Some(0)),
+        aoc::Problem::new(sample, Some(0)), // TODO: Fill in expected sample result
         aoc::Problem::new(input, None),
     ];
     let part_one = aoc::Solution::new("part_one", &part_one, &part_one_problems);
 
     let part_two_problems = [
-        aoc::Problem::new(sample, Some(0)),
+        aoc::Problem::new(sample, Some(0)), // TODO: Fill in expected sample result
         aoc::Problem::new(input, None),
     ];
     let part_two = aoc::Solution::new("part_two", &part_two, &part_two_problems);
